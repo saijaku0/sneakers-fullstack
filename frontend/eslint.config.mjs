f02ded44f7caaca46642f57abefe274072361f87
saijaku0 <minerking112256@gmail.com>
2025-12-04 15:41:10 +0100

    update package, make rules for fsd

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
+import tsParser from "@typescript-eslint/parser";
+
+const FSD_LAYERS = [
+  "app",
+  "pages",
+  "widgets",
+  "features",
+  "entities",
+  "shared",
+];
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +24,56 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser, 
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      import: importPlugin, 
+    },
+    rules: {
+      // Rules for import by layers FSD
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: FSD_LAYERS.map((layer) => ({
+            pattern: `@/${layer}/**`,
+            group: "internal",
+            position: "before",
+          })),
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      // Rules for correct imports
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
