fcff4d158a3e7104a92b024a5c0c337a86b81b31
saijaku0 <minerking112256@gmail.com>
2025-12-11 22:16:31 +0100

    Init backend

---
+++ backend/SneakersShop/SneakersShop.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SneakersShop.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
