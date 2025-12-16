27a1cd5274a24ecb81f75136f3c45944e18351ff
saijaku0 <minerking112256@gmail.com>
2025-12-16 19:49:35 +0100

    feat: add Sneakers and Brand DTO, make migrations

---
+++ backend/SneakersShop/SneakersShop.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SneakersShop.Domain.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastUpdatedAt { get; set; }
+        public string? LastUpdatedBy { get; set; }
+
+        public List<Sneaker> Sneakers { get; set; } = new();
+    }
+}
---
+++ backend/SneakersShop/SneakersShop.Domain/Entities/Sneaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SneakersShop.Domain.Entities
+{
+    public class Sneaker
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public string? LastUpdatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastUpdatedAt { get; set; }
+    }
+}
